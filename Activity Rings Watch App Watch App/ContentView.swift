f587462ca56bbfabe44f157936b6eab22c1396a3
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-19 01:27:28 +0600

    activity rings implemented

--- Activity Rings Watch App Watch App/ContentView.swift
+++ Activity Rings Watch App Watch App/ContentView.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            ActivityCircle(color: .red, pct: 0.7)
+                .padding(10)
+            ActivityCircle(color: .red.withAlphaComponent(0.3), pct: 1.0)
+                .padding(10)
+            //
+            ActivityCircle(color: .green, pct: 0.5)
+                .padding(25)
+            ActivityCircle(color: .green.withAlphaComponent(0.3), pct: 1.0)
+                .padding(25)
+            //
+            ActivityCircle(color: .cyan, pct: 0.8)
+                .padding(40)
+            ActivityCircle(color: .cyan.withAlphaComponent(0.3), pct: 1.0)
+                .padding(40)
         }
         .padding()
     }
@@ -21,4 +31,24 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+//Circle view
+struct ActivityCircle: View {
+    let color: UIColor
+    let pct: CGFloat
+    
+    var body: some View {
+        Circle()
+            .trim(from: 0, to: pct)
+            .rotation(Angle(degrees: -90))
+            .stroke(style: StrokeStyle(lineWidth: 13, lineCap: .round, lineJoin: .round))
+            .foregroundStyle(Color(color))
+    }
+}
+
+
+#Preview {
+    ActivityCircle(color: .blue, pct: 0.1)
 }
