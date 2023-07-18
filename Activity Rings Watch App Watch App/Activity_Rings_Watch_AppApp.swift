e20ef7af8673955de49b77a4effc3cc4dbc46104
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-19 01:09:22 +0600

    Initial Commit

---
+++ Activity Rings Watch App Watch App/Activity_Rings_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Activity_Rings_Watch_AppApp.swift
+//  Activity Rings Watch App Watch App
+//
+//  Created by Joynal Abedin on 19/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Activity_Rings_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
